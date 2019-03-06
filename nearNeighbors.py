2bf5bf9126ef6419474079533b3b141bc20d0ec8
metownsend <mindy.townsend@gmail.com>
2019-03-06 15:29:05 -0600

    Add existing file

--- nearNeighbors.py
+++ nearNeighbors.py
@@ -120,5 +120,5 @@ def nearNeighbor(distance, kpc_DA, ra_LRG, dec_LRG, ra_BKG, dec_BKG, rmag_LRG, r
                                                         normed=False)
             near.append(hist2d)
 
-    return (distance_kpc, near, gal_tree)
+    return (distance_kpc, near, gal_tree, dist)
     # return (near, gal_tree)
