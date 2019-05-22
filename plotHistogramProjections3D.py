3fe0a479e09ad291d1baaa05b0ab5503b54264bc
metownsend <mindy.townsend@gmail.com>
2019-05-22 07:25:28 -0500

    Add existing file

--- plotHistogramProjections3D.py
+++ plotHistogramProjections3D.py
@@ -14,25 +14,31 @@ def plotHistogramProjections3D(H, edges):
         gr_vs_rz = np.flipud(gr_v_rz.T)
 
         # plot 2D histograms using matshow; 3 plots per image
-        f, axarr = plt.subplots(3, 1, figsize=(15, 15))
+        f, axarr = plt.subplots(1, 3, figsize=(15, 10))
         f.suptitle("Nsat 2D Projections")
-        im1 = axarr[0].matshow(rz_vs_zmag, extent=[edges[1][0], edges[1][len(edges[1]) - 1], edges[0][0],
-                                                   edges[0][len(edges[0]) - 1]])
-        axarr[0].set_title("(r-z) vs zmag", pad=10)
+
+        im1 = axarr[0].matshow(rz_vs_zmag, aspect='equal', extent=[edges[1][0], edges[1][len(edges[1]) - 1], edges[0][0], edges[0][len(edges[0]) - 1]])
+        axarr[0].set_title("(r-z) vs zmag", pad=5)
+        axarr[0].xaxis.set_ticks_position('bottom')
         axarr[0].invert_xaxis()
-        plt.colorbar(im1, ax=axarr[0])
+        axarr[0].set(xlabel='zmag', ylabel='(r-z)')
+        plt.colorbar(im1, ax=axarr[0], fraction=0.08, pad=0.05)
 
-        im2 = axarr[1].matshow(gr_vs_zmag, extent=[edges[1][0], edges[1][len(edges[1]) - 1], edges[2][0],
-                                                   edges[2][len(edges[2]) - 1]])
-        axarr[1].set_title("(g-r) vs zmag", pad=10)
+        im2 = axarr[1].matshow(gr_vs_zmag, aspect='equal', extent=[edges[1][0], edges[1][len(edges[1]) - 1], edges[2][0], edges[2][len(edges[2]) - 1]])
+        axarr[1].set_title("(g-r) vs zmag", pad=5)
         axarr[1].invert_xaxis()
-        plt.colorbar(im2, ax=axarr[1])
+        axarr[1].xaxis.set_ticks_position('bottom')
+        axarr[1].set(xlabel='zmag', ylabel='(g-r)')
+        plt.colorbar(im2, ax=axarr[1], fraction=0.08, pad=0.05)
 
-        im3 = axarr[2].matshow(gr_vs_rz, extent=[edges[0][0], edges[0][len(edges[0]) - 1], edges[2][0],
-                                                 edges[2][len(edges[2]) - 1]])
-        axarr[2].set_title("(g-r) vs (r-z)",pad=10)
+        im3 = axarr[2].matshow(gr_vs_rz, aspect=1.85, extent=[edges[0][0], edges[0][len(edges[0]) - 1], edges[2][0], edges[2][len(edges[2]) - 1]])
+        axarr[2].set_title("(g-r) vs (r-z)", pad=5)
         axarr[2].invert_xaxis()
-        plt.colorbar(im3, ax=axarr[2])
+        axarr[2].xaxis.set_ticks_position('bottom')
+        axarr[2].set(xlabel='(r-z)', ylabel='(g-r)')
+        plt.colorbar(im3, ax=axarr[2], fraction=0.08, pad=0.05)
+
+        f.subplots_adjust(wspace=0.8)
 
         # save image with incrementing file name
         plt.savefig('/Users/mtownsend/anaconda/GitHub/lrg-project/Plots/LRG_science_plots/HistProjections/NsatProjections/Nsat3Dproj{}.jpeg'.format(i))
